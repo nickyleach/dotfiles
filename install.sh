7babe890a926b346a96e47222f4fd2de051f28b7
Nicky Leach <npl922@gmail.com>
2012-02-08 11:36:50 -0500

    Removed Solarized settings - not really working correctly

--- install.sh
+++ install.sh
@@ -7,15 +7,9 @@ cd "$(dirname "$0")"
 
 git pull origin master
 wget -q https://raw.github.com/git/git/master/contrib/completion/git-completion.bash -O .git-completion.bash
-wget -q https://raw.github.com/altercation/solarized/master/osx-terminal.app-colors-solarized/Solarized%20Dark%20ansi.terminal -O Solarized\ Dark.terminal
 
 rsync --quiet --exclude ".git/" --exclude ".DS_Store" --exclude "install.sh" --exclude "README.md" -av . ~
 
-rm .git-completion.bash Solarized\ Dark.terminal
+rm .git-completion.bash
 
 source ~/.bash_profile
-
-cd ~ 
-open Solarized\ Dark.terminal
-defaults write com.apple.Terminal "Startup Window Settings" -string "Solarized Dark"
-defaults write com.apple.Terminal "Startup Window Settings" -string "Solarized Dark"
